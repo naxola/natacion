a2759b93416c451b1f93a2f60a4e4db058f9e768
Nacho <naxola@hotmail.com>
2018-11-15 10:02:42 +0100

    front application

---
+++ admin-pro-angular/horizontal/src/app/dashboards/dashboard-components/social-slider/social-slider.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'social-slider',
+	templateUrl: './social-slider.component.html'
+})
+export class SocialSliderComponent {
+		 
+	constructor() {
+		
+	}
+
+	
+}
